90c0bf3598c95070033d38415fef4f41443d11d9
CyberNueng <nueng.42560@gmail.com>
2017-09-27 18:45:57 +0700

    Revert "v1.3"
    This reverts commit 1936b7e5be514f394fcf1e4441c9e76ab5e21ac5.

--- src/main/java/com/example/nuengeiei/cs355_asgn1/MainActivity2.java
+++ src/main/java/com/example/nuengeiei/cs355_asgn1/MainActivity2.java
@@ -2,8 +2,6 @@ package com.example.nuengeiei.cs355_asgn1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -11,26 +9,5 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        TextView txt1 = (TextView) findViewById(R.id.textView3);
-        String extra1 = getIntent().getStringExtra("extra1");
-        txt1.setText(extra1);
-        TextView txt2 = (TextView) findViewById(R.id.textView5);
-        String extra2 = getIntent().getStringExtra("extra2");
-        txt1.setText(extra2);
-        TextView txt3 = (TextView) findViewById(R.id.textView7);
-        String extra3 = getIntent().getStringExtra("extra3");
-        txt1.setText(extra3);
-        TextView txt4 = (TextView) findViewById(R.id.textView9);
-        String extra4 = getIntent().getStringExtra("extra4");
-        txt1.setText(extra4);
-        TextView txt5 = (TextView) findViewById(R.id.textView11);
-        String extra5 = getIntent().getStringExtra("extra5");
-        txt1.setText(extra5);
-        ImageView imgView= (ImageView)findViewById(R.id. imageView);
-        int age = Integer.parseInt(extra3);
-        if(age<=15) imgView.setImageResource(R.drawable.young);
-        else if(age<=25) imgView.setImageResource(R.drawable.teen);
-        else if(age<=60) imgView.setImageResource(R.drawable.working);
-        else imgView.setImageResource(R.drawable.old);
     }
 }
